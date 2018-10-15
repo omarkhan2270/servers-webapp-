9949bae3449efe4c16488e52d637a174a45ea59f
Makers Academy <onboarding@makersacademy.com>
2018-10-15 11:49:52 +0000

    loop and quit. second commit

--- server.rb
+++ server.rb
@@ -2,7 +2,12 @@ require 'socket'
 
 server = TCPServer.new(2345)
 socket = server.accept
-socket.puts "What do you say?"
-they_said = socket.gets.chomp
-socket.puts "You said #{they_said}. Goodbye"
+loop do
+  socket.puts "What do you say?"
+  they_said = socket.gets.chomp
+  socket.puts "You said #{they_said}. Goodbye"
+if they_said == 'quit'
+break
+end
+end 
 socket.close
